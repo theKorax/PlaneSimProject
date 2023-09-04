3d5c42513dbb39d994a99ff8223d02c81a2ede7e
theKorax <zikoramadin@gmail.com>
2023-09-04 14:47:47 +0100

    Update PlayerControllerX.cs
    Fixed a bug that made the player go backwards

--- Plane_Project/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Plane_Project/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed;
-    public float rotationSpeed;
+    public float speed = 10.0f;
+    public float rotationSpeed = 15.0f;
     public float verticalInput;
 
     // Start is called before the first frame update
@@ -15,13 +15,13 @@ public class PlayerControllerX : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         // get the user's vertical input
         verticalInput = Input.GetAxis("Vertical");
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.back * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
 
         // tilt the plane up/down based on up/down arrow keys
         transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
